9508e1c0f244756926e16fcd929942b79cc2abf8
Brian Kelly <polymonic@gmail.com>
2019-03-23 10:35:57 -0400

    Fix page numbering of Songbooks

--- app/jobs/generate_songbook_pdf_job.rb
+++ app/jobs/generate_songbook_pdf_job.rb
@@ -4,25 +4,23 @@ class GenerateSongbookPdfJob < ApplicationJob
   queue_as :default
 
   def perform(songbook)
-    pdf = CombinePDF.new
+    songs = songbook.songs.all.order(:title)
 
-    blank_page = CombinePDF.parse(songbook.blank_page.download)
+    pdf = CombinePDF.new
 
     pdf << CombinePDF.parse(songbook.table_of_contents.download)
 
-    songs = songbook.songs.all.order(:title)
-
     songs.order(:title).each do |song|
       pdf << CombinePDF.parse(song.pdf.download)
-      pdf << blank_page if song.page_count == 1
+      pdf << CombinePDF.parse(songbook.blank_page.download) if song.page_count == 1
     end
 
-    # pdf.number_pages(
-    #   number_format: '%s',
-    #   location: :bottom_right,
-    #   margin_from_height: 5,
-    #   font_size: 9
-    # )
+    pdf.number_pages(
+      number_format: '%s',
+      location: :bottom_right,
+      margin_from_height: 5,
+      font_size: 9
+    )
 
     songbook.pdf.attach(io: StringIO.new(pdf.to_pdf), filename: "#{songbook.name.parameterize}.pdf")
     songbook.update_column(:building, false)
